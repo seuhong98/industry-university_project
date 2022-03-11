76d2b481d5809ad11b197f3cf2cd5db1458e11eb
seuhong98 <79824139+seuhong98@users.noreply.github.com>
2022-03-11 14:46:10 +0900

    앱 화면 구성

---
+++ app/app/src/main/java/com/dongseung/studioapartmentreview/LoginPage.kt
@@ -0,0 +1,11 @@
+package com.dongseung.studioapartmentreview
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class LoginPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.page_login)
+    }
+}
