4f4f566a7c6870b14acd44c3691a7dd7adc1677c
seuhong98 <79824139+seuhong98@users.noreply.github.com>
2022-03-11 18:08:22 +0900

    Java로 변경

---
+++ app/app/src/main/java/com/dongseung/studioapartmentreview/LoginPage.java
@@ -0,0 +1,11 @@
+package com.dongseung.studioapartmentreview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginPage extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/app/src/main/java/com/dongseung/studioapartmentreview/Security.java
@@ -0,0 +1,5 @@
+package com.dongseung.studioapartmentreview;
+
+public class Security {
+    
+}
