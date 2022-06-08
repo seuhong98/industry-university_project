90b10f42921f995cedd59109075d8ae9348763be
seuhong98 <79824139+seuhong98@users.noreply.github.com>
2022-06-08 13:11:43 +0900

    boot로 변경_gradle

--- API_Server/src/test/java/com/oc/api_server/ApiServerApplicationTests.java
+++ API_Server/src/test/java/com/oc/api_server/ApiServerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.OC.OC;
+package com.oc.api_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OcApplicationTests {
+class ApiServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
